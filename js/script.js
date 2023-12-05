24dada9c5a6072ac01c22847edd0e25ddd387b44
Eduardo Kramer <dududeoliveirakramer23@gmail.com>
2023-12-05 19:34:13 -0300

    iniciando a função de click da celula

--- js/script.js
+++ js/script.js
@@ -20,33 +20,75 @@
 // });
 
 let gameState = {
-    board: [],
-    gameStarted: false,
-    player: 'X',
-    gameOver: false,
-    winner: ''
+  size: 3,
+  board: [],
+  gameStarted: false,
+  player: "X",
+  gameOver: false,
+  winner: "",
 };
 
 let boardGame = [];
 
 const generateBoardGame = () => {
-    if (gameState.gameStarted) {
-        return;
-    }
+  if (gameState.gameStarted) {
+    return;
+  }
 
-    gameState.gameStarted = true;
+  gameState.gameStarted = true;
 
-    let boardSize = document.getElementById('boardSize').value;
-    const boardGame = document.getElementById('boardGame')
+  gameState.size = document.getElementById("boardSize").value;
+  const boardGame = document.getElementById("boardGame");
 
-    boardGame.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
-    boardGame.style.gridTemplateRows  = `repeat(${boardSize}, 1fr)`;
+  boardGame.style.gridTemplateColumns = `repeat(${gameState.size}, 1fr)`;
+  boardGame.style.gridTemplateRows = `repeat(${gameState.size}, 1fr)`;
 
-    for (let i = 0; i < boardSize * boardSize; i++) {
-        gameState.board.push('');
-        let div = document.createElement('div');
-        div.classList.add('square');
-        div.setAttribute('id', `square-${i}`);
-        boardGame.appendChild(div);
-    }
+  gameState.board = [];
+
+  for (let i = 0; i < gameState.size * gameState.size; i++) {
+    gameState.board.push("");
+    let div = document.createElement("div");
+    div.classList.add("square");
+    div.setAttribute("id", `square-${i}`);
+    boardGame.appendChild(div);
+  }
+
+  const cell = document.querySelectorAll("div.square").forEach((item) => {
+    item.addEventListener("click", newMove);
+  });
 };
+
+function newMove(e) {
+  const index = e.target.getAttribute("data-i");
+  e.target.innerHTML = gameState.player;
+  e.target.removeEventListener('click', newMove);
+}
+
+/* const handleCellClick = (index) => {
+  if (gameState.gameOver || gameState.board[index] !== "") {
+    return;
+  }
+
+  const currentPlayer = gameState.player;
+  const cell = document.getElementById(`square-${index}`);
+
+  placeMark(cell, currentPlayer);
+  gameState.board[index] = currentPlayer;
+
+  placeMark(cell, currentPlayer);
+  gameState.board[index] = currentPlayer;
+
+  const isWin = checkForWin(currentPlayer);
+  const isDraw = checkForDraw();
+
+  if (isWin || isDraw) {
+    endGame(isDraw);
+  } else {
+    swapTurns();
+  }
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.textContent = classToAdd; // Adiciona 'X' ou 'O' ao conteúdo da célula
+  cell.classList.add(classToAdd); // Adiciona a classe para estilização
+}; */
